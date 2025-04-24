34bd1ee9839f428fe0e7114bbae61a08b10ea8e4
Ruba <s12028675@stu.najah.edu>
2025-04-25 00:05:24 +0300

    Create ServerStatistics class

---
+++ Server_Statistics_Collection_Service/ServerStatistics.cs
@@ -0,0 +1,9 @@
+namespace Server_Statistics_Collection_Service;
+
+public class ServerStatistics
+{
+    public double MemoryUsage { get; set; } // in MB
+    public double AvailableMemory { get; set; } // in MB
+    public double CpuUsage { get; set; }
+    public DateTime Timestamp { get; set; }
+}
