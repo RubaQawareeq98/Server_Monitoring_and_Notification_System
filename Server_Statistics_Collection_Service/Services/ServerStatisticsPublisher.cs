92d5170b00b219c66527232d2bd3afdc6965d451
Ruba <s12028675@stu.najah.edu>
2025-04-26 01:35:07 +0300

    Refactor ServerStatisticsPublisher: Add delay interval

--- Server_Statistics_Collection_Service/Services/ServerStatisticsPublisher.cs
+++ Server_Statistics_Collection_Service/Services/ServerStatisticsPublisher.cs
@@ -1,14 +1,25 @@
 using Server_Statistics_Collection_Service.Configurations;
+using Server_Statistics_Collection_Service.Models;
+using Server_Statistics_Collection_Service.Serializers;
 using Server_Statistics_Collection_Service.Services.Interfaces;
 
 namespace Server_Statistics_Collection_Service.Services;
 
-public class ServerStatisticsPublisher (IStatisticsCollector collector, IMessagePublisher publisher, ServerStatisticsConfig config) : IServerStatisticsPublisher
+public class ServerStatisticsPublisher (
+    IStatisticsCollector collector,
+    IMessagePublisher publisher,
+    ISerializer<ServerStatistics> serializer,
+    ServerStatisticsConfig config) : IServerStatisticsPublisher
 {
     public async Task RunAsync()
     {
-        var statistics = collector.CollectServerStatistics();
-       // await publisher.Publish(statistics);
+        for (var i = 0; i < 100; i++)
+        {
+            var statistics = collector.CollectServerStatistics();
+            var message = serializer.Serialize(statistics);
+            await publisher.Publish(message);
 
+            await Task.Delay(config.SamplingIntervalSeconds * 1000);
+        }
     }
 }
