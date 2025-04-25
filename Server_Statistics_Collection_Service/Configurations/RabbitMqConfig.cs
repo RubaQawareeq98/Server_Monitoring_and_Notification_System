058d19b946ca2d983b56728bc40fa3219eb29bd2
Ruba <s12028675@stu.najah.edu>
2025-04-26 01:06:16 +0300

    Refactor RabbitMqConfig: Add UserName & Password

--- Server_Statistics_Collection_Service/Configurations/RabbitMqConfig.cs
+++ Server_Statistics_Collection_Service/Configurations/RabbitMqConfig.cs
@@ -3,4 +3,6 @@ namespace Server_Statistics_Collection_Service.Configurations;
 public class RabbitMqConfig
 {
     public required string HostName { get; set; }
+    public required string UserName { get; set; }
+    public required string Password { get; set; }
 }
