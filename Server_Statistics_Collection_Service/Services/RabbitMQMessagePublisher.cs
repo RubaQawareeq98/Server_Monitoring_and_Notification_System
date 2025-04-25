153d5ae1008666121f9c16df4a313b8808ff98a9
Ruba <s12028675@stu.najah.edu>
2025-04-26 01:12:37 +0300

    Refactor RabbitMQMessagePublisher: Replace hostName parameter with RabbitMqConfig object

--- Server_Statistics_Collection_Service/Services/RabbitMQMessagePublisher.cs
+++ Server_Statistics_Collection_Service/Services/RabbitMQMessagePublisher.cs
@@ -1,13 +1,14 @@
 using System.Text;
 using Server_Statistics_Collection_Service.Services.Interfaces;
 using RabbitMQ.Client;
+using Server_Statistics_Collection_Service.Configurations;
 
 namespace Server_Statistics_Collection_Service.Services;
 
-public class RabbitMqMessagePublisher(string hostName, string serverIdentifier) : IMessagePublisher
+public class RabbitMqMessagePublisher(RabbitMqConfig config, string serverIdentifier) : IMessagePublisher
 {
-    private readonly IConnectionFactory _factory = new ConnectionFactory { HostName = hostName };
-    private readonly string _message = $"Server {serverIdentifier} is running on host {hostName}";
+    private readonly IConnectionFactory _factory = new ConnectionFactory { HostName = config.HostName, UserName = config.UserName, Password = config.Password};
+    private readonly string _topic = $"Server {serverIdentifier} is running";
 
     public async Task Publish(string message)
     {
@@ -15,14 +16,15 @@ public class RabbitMqMessagePublisher(string hostName, string serverIdentifier) 
         await using var channel = await connection.CreateChannelAsync();
 
         var basicProperties = new BasicProperties();
-        
+
         var body = Encoding.UTF8.GetBytes(message);
 
         await channel.BasicPublishAsync(
-            exchange: string.Empty,
-            routingKey: this._message,
-            mandatory: true,
+            exchange: string.Empty,   
+            routingKey: _topic,   
+            mandatory: true,       
             basicProperties: basicProperties,
-            body: body);
+            body: body
+        );
     }
 }
