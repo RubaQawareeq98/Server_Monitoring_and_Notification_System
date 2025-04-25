8c7b72192e738e9d77c9935eb9511d16fcccb295
Ruba <s12028675@stu.najah.edu>
2025-04-26 02:04:18 +0300

    Configure Dependency Injection

--- Server_Statistics_Collection_Service/Program.cs
+++ Server_Statistics_Collection_Service/Program.cs
@@ -1,32 +1,44 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Server_Statistics_Collection_Service.Configurations;
-using Server_Statistics_Collection_Service.Factories;
-using Server_Statistics_Collection_Service.Factories.Interfaces;
+using Server_Statistics_Collection_Service.Models;
+using Server_Statistics_Collection_Service.Serializers;
+using Server_Statistics_Collection_Service.Services;
+using Server_Statistics_Collection_Service.Services.Interfaces;
 using Server_Statistics_Collection_Service.Strategies;
 
 namespace Server_Statistics_Collection_Service;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         var configuration = new ConfigurationBuilder()
             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
             .Build();
-        
-        var settings = configuration.GetSection("ServerStatisticsConfig").Get<ServerStatisticsConfig>();
-      
-        IServerStatisticsCollectorFactory factory = new ServerStatisticsCollectorFactory();
-        var statistic = factory._serverStatisticsCollector(PlatformID.Unix);
-        Console.WriteLine("statistic");
-         var availableMemory = statistic.GetAvailableMemory();
-         var usedMemory = statistic.GetMemoryUsage();
-         var usageCpu = statistic.GetCpuUsage();
-         var time = statistic.GetTimestamp();
-        Console.WriteLine($"Available Memory: {availableMemory}");
-        Console.WriteLine($"Used Memory: {usedMemory}");
-        Console.WriteLine($"Usage Cpu: {usageCpu}");
-        Console.WriteLine($"Time: {time}");
 
+        var service = new ServiceCollection();
+        
+        var serverStatisticsConfig = configuration.GetSection("ServerStatisticsConfig").Get<ServerStatisticsConfig>();
+        var rabbitMqConfig = configuration.GetSection("RabbitMQConfig").Get<RabbitMqConfig>();
+
+        if (serverStatisticsConfig is null || rabbitMqConfig is null)
+        {
+            Console.WriteLine("No server statistics config found.");
+            return;
+        }
+        
+        service.AddSingleton(serverStatisticsConfig);
+        service.AddSingleton(rabbitMqConfig);
+        service.AddSingleton<IServerStatisticsCollector, WindowsServerStatisticsCollector>();
+        service.AddSingleton<IStatisticsCollector, StatisticsCollector>();
+        service.AddSingleton<IMessagePublisher, RabbitMqMessagePublisher>();
+        service.AddSingleton<ISerializer<ServerStatistics>, JsonSerializer<ServerStatistics>>();
+        service.AddSingleton<IServerStatisticsPublisher, ServerStatisticsPublisher>();
+        
+        var provider = service.BuildServiceProvider();
+        var publisher = provider.GetService<IServerStatisticsPublisher>();
+        
+        await publisher?.RunAsync();
     }
 }
