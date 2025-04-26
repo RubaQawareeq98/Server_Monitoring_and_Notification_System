a001dbacb2737241922ee3918ed5bb865c0026ba
Ruba <s12028675@stu.najah.edu>
2025-04-26 03:24:51 +0300

    Register IServerStatisticsCollectorFactory service

--- Server_Statistics_Collection_Service/Extensions/AddServicesDependency.cs
+++ Server_Statistics_Collection_Service/Extensions/AddServicesDependency.cs
@@ -1,17 +1,18 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Server_Statistics_Collection_Service.Configurations;
+using Server_Statistics_Collection_Service.Factories;
+using Server_Statistics_Collection_Service.Factories.Interfaces;
 using Server_Statistics_Collection_Service.Models;
 using Server_Statistics_Collection_Service.Serializers;
 using Server_Statistics_Collection_Service.Services;
 using Server_Statistics_Collection_Service.Services.Interfaces;
-using Server_Statistics_Collection_Service.Strategies;
 
 namespace Server_Statistics_Collection_Service.Extensions;
 
 public static class AddServicesDependency
 {
-    public static void AddServices(this IServiceCollection service, IConfiguration configuration)
+    public static void AddServices(this IServiceCollection services, IConfiguration configuration)
     {
         var serverStatisticsConfig = configuration.GetSection("ServerStatisticsConfig").Get<ServerStatisticsConfig>();
         var rabbitMqConfig = configuration.GetSection("RabbitMQConfig").Get<RabbitMqConfig>();
@@ -22,12 +23,17 @@ public static class AddServicesDependency
             return;
         }
         
-        service.AddSingleton(serverStatisticsConfig);
-        service.AddSingleton(rabbitMqConfig);
-        service.AddSingleton<IServerStatisticsCollector, WindowsServerStatisticsCollector>();
-        service.AddSingleton<IStatisticsCollector, StatisticsCollector>();
-        service.AddSingleton<IMessagePublisher, RabbitMqMessagePublisher>();
-        service.AddSingleton<ISerializer<ServerStatistics>, JsonSerializer<ServerStatistics>>();
-        service.AddSingleton<IServerStatisticsPublisher, ServerStatisticsPublisher>();
+        services.AddSingleton(serverStatisticsConfig);
+        services.AddSingleton(rabbitMqConfig);
+        services.AddSingleton<IServerStatisticsCollectorFactory, ServerStatisticsCollectorFactory>();
+        services.AddSingleton<IServerStatisticsCollector>(provider =>
+        {
+            var factory = provider.GetRequiredService<IServerStatisticsCollectorFactory>();
+            return factory.GetServerStatisticsCollector(PlatformID.Win32S);
+        });
+        services.AddSingleton<IStatisticsCollector, StatisticsCollector>();
+        services.AddSingleton<IMessagePublisher, RabbitMqMessagePublisher>();
+        services.AddSingleton<ISerializer<ServerStatistics>, JsonSerializer<ServerStatistics>>();
+        services.AddSingleton<IServerStatisticsPublisher, ServerStatisticsPublisher>();
     }
 }
