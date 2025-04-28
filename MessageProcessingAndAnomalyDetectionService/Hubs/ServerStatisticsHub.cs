698ed6d967cfba4a5cdfc3b7fafb6365c48fdfa8
Ruba <s12028675@stu.najah.edu>
2025-04-29 01:36:59 +0300

    Refactor ServerStatisticsHub: Pass SignalRConfig object instead of string & add Logger

--- MessageProcessingAndAnomalyDetectionService/Hubs/ServerStatisticsHub.cs
+++ MessageProcessingAndAnomalyDetectionService/Hubs/ServerStatisticsHub.cs
@@ -1,10 +1,12 @@
+using MessageProcessingAndAnomalyDetectionService.Configurations;
 using MessageProcessingAndAnomalyDetectionService.Hubs.Interfaces;
 using MessageProcessingAndAnomalyDetectionService.Models;
 using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Logging;
 
 namespace MessageProcessingAndAnomalyDetectionService.Hubs;
 
-public class ServerStatisticsHub (string signalRUrl) : IServerStatisticsHub
+public class ServerStatisticsHub (SignalRConfig signalRConfig, ILogger logger) : IServerStatisticsHub
 {
     public async Task SendAlertAsync(List<Alert> alerts)
     {
@@ -12,13 +14,18 @@ public class ServerStatisticsHub (string signalRUrl) : IServerStatisticsHub
         {
             return;
         }
-
+        logger.LogInformation("Sending alert to hub");
+        
         var connection = new HubConnectionBuilder()
-            .WithUrl(signalRUrl)
+            .WithUrl(signalRConfig.SignalRUrl)
             .Build();
 
         await connection.StartAsync();
-        await connection.InvokeAsync("SendAlert", alerts);
+        
+        foreach (var a in alerts)
+        {
+            await connection.InvokeCoreAsync("SendAlert", [nameof(a.AlertType), a.Message]);
+        }
         await connection.DisposeAsync();
     }
 }
