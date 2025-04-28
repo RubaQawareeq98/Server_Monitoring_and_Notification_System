3a41b5233d660782224ce7716038b161673482f2
Ruba <s12028675@stu.najah.edu>
2025-04-28 04:17:05 +0300

    Extract Channel Creation to Factory Method

---
+++ ServerStatisticsCollectionService/Factories/ChannelFactory.cs
@@ -0,0 +1,29 @@
+using RabbitMQ.Client;
+using ServerStatisticsCollectionService.Configurations;
+using ServerStatisticsCollectionService.Factories.Interfaces;
+
+namespace ServerStatisticsCollectionService.Factories;
+
+public class ChannelFactory (RabbitMqConfig rabbitMqConfig) : IChannelFactory
+{
+    public async Task<IChannel> GetChannel()
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = rabbitMqConfig.HostName,
+            UserName = rabbitMqConfig.UserName,
+            Password = rabbitMqConfig.Password
+        };
+        
+        var connection = await factory.CreateConnectionAsync();
+        var channel = await connection.CreateChannelAsync();
+        
+        await channel.ExchangeDeclareAsync(
+            exchange: rabbitMqConfig.Exchange,
+            type: ExchangeType.Topic,
+            durable: true
+        );
+        
+        return channel;
+    }
+}
