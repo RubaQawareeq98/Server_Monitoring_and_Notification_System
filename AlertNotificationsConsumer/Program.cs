b1f7e64460faf2f0b537abc484c63c5852f93f92
Ruba <s12028675@stu.najah.edu>
2025-04-29 03:16:23 +0300

    Refactor AlertNotificationsConsumer Project: Apply DI

--- AlertNotificationsConsumer/Program.cs
+++ AlertNotificationsConsumer/Program.cs
@@ -1,18 +1,44 @@
 ﻿using AlertNotificationsConsumer.Services;
 using AlertNotificationsConsumer.Services.Interfaces;
+using Domain.Configurations;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
 
 namespace AlertNotificationsConsumer;
 
-class Program
+abstract class Program
 {
-    static async Task Main(string[] args)
+    static async Task Main()
     {
-        Console.WriteLine("Hello, World!");
-        var sig = "http://localhost:5199/alertHub";
-        ISignalRAlertConsumerService consumerService = new SignalRAlertConsumerService(sig);
-        await consumerService.ConsumeAlertsAsync();
-        Console.WriteLine("Press any key to exit...");
-        Console.ReadKey();
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        var services = new ServiceCollection();
+
+        services.Configure<SignalRConfig>(configuration.GetSection("SignalRConfig"));
+        services.AddLogging(configure => configure.AddConsole().SetMinimumLevel(LogLevel.Information));
+
+        
+        services.AddSingleton<ISignalRAlertConsumerService, SignalRAlertConsumerService>();
+        
+        var serviceProvider = services.BuildServiceProvider();
+        try
+        {
+            var consumerService = serviceProvider.GetService<ISignalRAlertConsumerService>();
+            if (consumerService != null) await consumerService.ConsumeAlertsAsync();
+            var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+
+            logger.LogInformation("Press any key to exit...");
+            Console.ReadKey();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        
 
     }
 }
--- AlertNotificationsConsumer/Services/SignalRAlertConsumerService.cs
+++ AlertNotificationsConsumer/Services/SignalRAlertConsumerService.cs
@@ -1,23 +1,27 @@
 using AlertNotificationsConsumer.Services.Interfaces;
+using Domain.Configurations;
 using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace AlertNotificationsConsumer.Services;
 
-public class SignalRAlertConsumerService(string signalRUrl) : ISignalRAlertConsumerService
+public class SignalRAlertConsumerService(IOptions<SignalRConfig> options, ILogger<SignalRAlertConsumerService> logger) : ISignalRAlertConsumerService
 {
     public async Task ConsumeAlertsAsync()
     {
+        var signalRUrl = options.Value.SignalRUrl;
         var hubConnection = new HubConnectionBuilder()
             .WithUrl(signalRUrl)
             .WithAutomaticReconnect()
             .Build();
 
-        hubConnection.On<string, string>("ReceiveAlert", (title, message) =>
+        hubConnection.On<string, string>("ReceiveAlert", (alertType, message) =>
         {
-            Console.WriteLine($"Received Alert: {title} - {message}");
+            logger.LogInformation("Received Alert: {AlertType} - {Message}", alertType, message);
         });
         
         await hubConnection.StartAsync();
-        Console.WriteLine("Connected to SignalR Hub.");
+        logger.LogInformation("Connected to SignalR Hub.");
     }
 }
